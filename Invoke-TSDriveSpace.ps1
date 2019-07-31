641c18fde6f6d7e2a6b243474152cebd51c3bb13
Tim Davis <binary.god@gmail.com>
2019-07-31 15:43:49 -0400

    Testing serializeation

--- Invoke-TSDriveSpace.ps1
+++ Invoke-TSDriveSpace.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 
 .DESCRIPTION
-    Drive Space Collection for CHM
+    Drive Space Collection for Truesec Client Health Monitor
 .LINK
 
 .NOTES
@@ -26,6 +26,31 @@
 [cmdletbinding()]
 param()
 begin{
-    $ExtensionID = "ff48135e-4764-405f-8bc8-8e83ded065e3"
+    Import-Module ClientHealthCore
+    $ExtensionID = "d3d4bf2d-fd70-4e34-bccd-16986197e0f4"
 }
-process{}
+process{
+    
+
+    # Define Model
+    $Model = new-object PSCustomObject 
+
+    # Mock data, important to define datatype
+    # UUID links data to the Computer, should be kept
+    [GUID]$UUID = "FF48135E-4764-405F-8BC8-8E83DED065E3"
+    # Add more properties as needed
+    [String]$DeviceID = "C:"
+    [UInt64]$Size = 499530067968
+    [uint64]$Free = 209478905856
+    [String]$VolumeName = "Boot"
+
+    # Add Properties to the model, these get translated to Database Columns
+    $Model | Add-Member NoteProperty UUID $UUID
+    # Add more properties as needed
+    $Model | Add-Member NoteProperty DeviceID $DeviceID
+    $Model | Add-Member NoteProperty Size $Size
+    $Model | Add-Member NoteProperty Free $Free
+    $Model | Add-Member NoteProperty VolumneName $VolumeName
+
+    Send-CHClientData -ExtensionID $ExtensionID -Data $Model -Table "TSDriveSpace"
+}
